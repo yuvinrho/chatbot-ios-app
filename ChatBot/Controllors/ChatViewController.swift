3bf1642482e6efc5ac6c3abe7a410082a1a88179
yuvinrho <yvrho@naver.com>
2023-05-10 23:47:05 +0900

    feat: chat ui 설정

--- ChatBot/Controllors/ChatViewController.swift
+++ ChatBot/Controllors/ChatViewController.swift
@@ -7,13 +7,57 @@
 
 import UIKit
 
-class ChatViewController: UIViewController {
+final class ChatViewController: UIViewController {
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+
+    private let textField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "Send a message."
+        textField.backgroundColor = .systemYellow
+        textField.rightViewMode = .always
+        return textField
+    }()
+
+    private let sendButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(systemName: "paperplane"), for: .normal)
+        button.isEnabled = false
+        return button
+    }()
+
+    private let messages: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        configureNavigation()
+        configureView()
     }
 
+    private func configureNavigation() {
+        navigationItem.title = "chat"
+    }
 
+    private func configureView() {
+        view.addSubview(tableView)
+        view.addSubview(textField)
+
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -8),
+            tableView.bottomAnchor.constraint(equalTo: textField.topAnchor),
+
+            textField.heightAnchor.constraint(equalToConstant: 50),
+            textField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8),
+            textField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -8),
+            textField.bottomAnchor.constraint(equalTo: view.keyboardLayoutGuide.topAnchor),
+        ])
+    }
 }
-
