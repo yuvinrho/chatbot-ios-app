5740e20a9eb65e0363d1e9e4461f1ae60efbb48c
yuvinrho <yvrho@naver.com>
2023-05-15 20:57:19 +0900

    refactor: ChatViewController 뷰 분리

--- ChatBot/Controllors/ChatViewController.swift
+++ ChatBot/Controllors/ChatViewController.swift
@@ -2,49 +2,14 @@ import UIKit
 
 final class ChatViewController: UIViewController {
     // MARK: Properties
+    private let chatView: ChatView = {
+        let view = ChatView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
     private let chatManager = ChatManager()
     private var messages: [String] = []
-
-    private let tableView: UITableView = {
-        let tableView = UITableView()
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        tableView.backgroundColor = .systemGray6
-        return tableView
-    }()
-
-    private let stackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.axis = .horizontal
-        stackView.alignment = .center
-        stackView.spacing = 8
-        return stackView
-    }()
-
-    private let textView: UITextView = {
-        let textView = UITextView()
-        textView.font = UIFont.systemFont(ofSize: 17)
-        textView.backgroundColor = .systemGray3
-        textView.layer.cornerRadius = 10
-        textView.text = " "
-        textView.textColor = .black
-        return textView
-    }()
-
-    private let sendButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setImage(UIImage(systemName: "paperplane"), for: .normal)
-        button.isEnabled = false
-        return button
-    }()
-
-    private let loadingIndicator: UIActivityIndicatorView = {
-        let indicator = UIActivityIndicatorView(style: .large)
-        indicator.startAnimating()
-        indicator.isHidden = true
-        return indicator
-    }()
 
     // MARK: View Life Cycle
     override func viewDidLoad() {
@@ -52,7 +17,6 @@ final class ChatViewController: UIViewController {
 
         configureNavigation()
         configureView()
-        configureConstraints()
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -64,54 +28,39 @@ final class ChatViewController: UIViewController {
         navigationItem.title = "AI Chat"
     }
 
-    private func configureConstraints() {
-        view.addSubview(tableView)
-        view.addSubview(stackView)
+    private func configureView() {
+        view.addSubview(chatView)
 
         NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8),
-            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -8),
-            tableView.bottomAnchor.constraint(equalTo: textView.topAnchor, constant: -8),
-
-            stackView.topAnchor.constraint(equalTo: tableView.bottomAnchor),
-            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8),
-            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -8),
-            stackView.bottomAnchor.constraint(equalTo: view.keyboardLayoutGuide.topAnchor),
-
-            textView.heightAnchor.constraint(equalToConstant: textView.contentSize.height)
+            chatView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            chatView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8),
+            chatView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -8),
+            chatView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
         ])
-    }
 
-    private func configureView() {
-        tableView.dataSource = self
-        textView.delegate = self
-
-        sendButton.addAction(UIAction(handler: { [weak self] action in
+        chatView.setTextViewDelegate(delegate: self)
+        chatView.setTableViewDataSource(dataSource: self)
+        chatView.setSendButtonAction(action: UIAction(handler: { [weak self] action in
             self?.touchUpSendButton()
-        }), for: .touchUpInside)
-
-        [textView, sendButton, loadingIndicator].forEach {
-            stackView.addArrangedSubview($0)
-        }
-    }
-
-    private func clearInputMessage() {
-        textView.text = nil
-        toggleSendButton()
-        resizeTextViewHeight()
+        }))
     }
 
     private func touchUpSendButton() {
-        guard let inputMessage = textView.text else {
+        guard let inputMessage = chatView.inputMessage?.trimmingCharacters(in: .whitespacesAndNewlines) else {
             return
         }
 
         messages.append(inputMessage)
-        updateTableView()
-        updateTextView()
-        showLoadingIndicator(true)
+        updateChatView()
         sendMessage(inputMessage)
+    }
+
+    private func updateChatView() {
+        chatView.updateTableView()
+        chatView.clearTextView()
+        chatView.resizeTextViewHeight()
+        chatView.updateSendButtonState()
+        chatView.setLoadingIndicatorVisible(true)
     }
 
     private func sendMessage(_ message: String) {
@@ -124,55 +73,9 @@ final class ChatViewController: UIViewController {
             }
 
             DispatchQueue.main.async {
-                self?.updateTableView()
-                self?.showLoadingIndicator(false)
+                self?.chatView.updateTableView()
+                self?.chatView.setLoadingIndicatorVisible(false)
             }
-        }
-    }
-
-    private func updateTextView() {
-        textView.resignFirstResponder()
-        clearInputMessage()
-    }
-
-    private func updateTableView() {
-        self.tableView.reloadData()
-        let lastRowIndex = self.tableView.numberOfRows(inSection: 0) - 1
-        let lastIndexPath = IndexPath(row: lastRowIndex, section: 0)
-        self.tableView.scrollToRow(at: lastIndexPath, at: .bottom, animated: true)
-    }
-
-    private func showLoadingIndicator(_ flag: Bool) {
-        loadingIndicator.isHidden = !flag
-        sendButton.isHidden = flag
-    }
-
-    // 입력한 메시지 크기에 따라 텍스트뷰 높이 동적으로 조절
-    private func resizeTextViewHeight() {
-        let maxSize = view.frame.height / 4
-        let size = CGSize(width: textView.frame.width, height: .infinity)
-        let estimatedSize = textView.sizeThatFits(size)
-
-        textView.constraints.forEach { constraint in
-            if constraint.firstAttribute == .height {
-                if estimatedSize.height >= maxSize {
-                    constraint.constant = maxSize
-                    return
-                }
-
-                constraint.constant = estimatedSize.height
-            }
-        }
-    }
-
-    // 입력한 메시지가 있으면 버튼 활성화, 없으면 비활성화
-    private func toggleSendButton() {
-        if let inputMessage = textView.text?.trimmingCharacters(in: .whitespacesAndNewlines),
-           !inputMessage.isEmpty,
-           inputMessage != "" {
-            sendButton.isEnabled = true
-        } else {
-            sendButton.isEnabled = false
         }
     }
 }
@@ -184,16 +87,7 @@ extension ChatViewController: UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = messages[indexPath.row]
-        cell.textLabel?.numberOfLines = 0
-
-        if indexPath.row % 2 == 0 {
-            cell.contentView.backgroundColor = .systemGray4
-        } else {
-            cell.contentView.backgroundColor = .white
-        }
-
+        let cell = chatView.configureCell(with: messages, indexPath: indexPath)
         return cell
     }
 }
@@ -202,9 +96,9 @@ extension ChatViewController: UITableViewDataSource {
 extension ChatViewController: UITextViewDelegate {
     func textViewDidChange(_ textView: UITextView) {
         // 텍스트 라인에 따라 텍스트뷰 크기 동적으로 변경
-        resizeTextViewHeight()
-        // 텍스트 비어있는지 여부에 따라 sendButton 토글
-        toggleSendButton()
+        chatView.resizeTextViewHeight()
+        // 텍스트 비어있는지 여부에 따라 sendButton 상태 업데이트
+        chatView.updateSendButtonState()
     }
 
     func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
